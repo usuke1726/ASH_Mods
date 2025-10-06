fe385ec1d27a9d212e17465306b61ba5a9464efe
usuke1726 <78670588+usuke1726@users.noreply.github.com>
2025-10-06 23:15:40 +0900

    added lines of rubberflower dialogue, modified item descriptions

---
+++ SideStory/NodeData/RubberFlower.cs
@@ -0,0 +1,24 @@
+﻿
+using SideStory.Dialogue;
+using SideStory.System;
+
+namespace SideStory.NodeData;
+internal class RubberFlower : StartNodeEntry
+{
+    protected override string StartNode => "BucketSignStart";
+    private static readonly string viewedOnceTag = "Node.RubberFlowerSignViewed";
+    protected override Node[] Nodes => [new([
+        line("node.sign.bucketsign1", Original),
+        @if(() => STags.GetBool(viewedOnceTag), "1", null),
+        line("node.sign.bucketsign2", Original),
+        line("node.sign.bucketsign3", Original, anchor: "1"),
+        line("node.sign.bucketsign4", Original),
+        @if(() => STags.GetBool(viewedOnceTag), "2", null),
+        line("node.sign.bucketsign5", Original),
+        line("node.sign.bucketsign6", Original),
+        ..range(7, 11).Select(i => line($"node.sign.bucketsign{i}", Player)),
+        line("node.sign.bucketsign12", Player, anchor: "2"),
+        tag(viewedOnceTag, true)
+    ])];
+}
+
