e754291cb633c9572c111f8f75da86f4a1439977
usuke1726 <78670588+usuke1726@users.noreply.github.com>
2025-10-07 14:18:23 +0900

    modified node data

--- SideStory/NodeData/RubberFlower.cs
+++ SideStory/NodeData/RubberFlower.cs
@@ -16,7 +16,7 @@ internal class RubberFlower : StartNodeEntry
         @if(() => STags.GetBool(viewedOnceTag), "2", null),
         line("node.sign.bucketsign5", Original),
         line("node.sign.bucketsign6", Original),
-        ..range(7, 11).Select(i => line($"node.sign.bucketsign{i}", Player)),
+        lines(7, 11, i => $"node.sign.bucketsign{i}", Player),
         line("node.sign.bucketsign12", Player, anchor: "2"),
         tag(viewedOnceTag, true)
     ])];
