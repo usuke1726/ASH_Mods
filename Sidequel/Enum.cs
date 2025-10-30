518bb6d6c2688b728f64b6cda47b71c958ef82e2
usuke1726 <78670588+usuke1726@users.noreply.github.com>
2025-10-30 22:03:40 +0900

    added items and modifications of i18n

--- Sidequel/Enum.cs
+++ Sidequel/Enum.cs
@@ -11,6 +11,26 @@ internal static class Items
     public static readonly string Pickaxe = "Pickaxe";
     public static readonly string Fish = "Fish";
     public static readonly string Bucket = "Bucket";
+    public static readonly string WalkieTalkie = "WalkieTalkie";
+    public static readonly string Compass = "Compass";
+    public static readonly string RunningShoes = "RunningShoes";
+
+    // New items
+    public static readonly string GoldMedal = "GoldMedal";
+    public static readonly string SouvenirMedal = "SouvenirMedal";
+    public static readonly string FishHook = "FishHook";
+    public static readonly string AntiqueFigure = "AntiqueFigure";
+    public static readonly string OldPicture = "OldPicture";
+    public static readonly string CuteEmptyCan = "CuteEmptyCan";
+    public static readonly string TradingCard = "TradingCard";
+    public static readonly string StrongSunscreen = "StrongSunscreen";
+    public static readonly string WeakSunscreen = "WeakSunscreen";
+    public static readonly string Sunscreen = "Sunscreen";
+    public static readonly string HalfUsedSunscreen = "HalfUsedSunscreen";
+    public static readonly string JimsAddressNote = "JimsAddressNote";
+    public static readonly string FishScale1 = "FishScale1";
+    public static readonly string FishScale2 = "FishScale2";
+    public static readonly string FishScale3 = "FishScale3";
 }
 
 public enum Emotes
