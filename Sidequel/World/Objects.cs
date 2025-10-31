f8aead145b2353857fcca2898bf5a4c161e9ed74
usuke1726 <78670588+usuke1726@users.noreply.github.com>
2025-10-31 10:18:00 +0900

    added features to remove world items and initialize climber npcs

--- Sidequel/World/Objects.cs
+++ Sidequel/World/Objects.cs
@@ -18,6 +18,50 @@ internal class Objects
         if (!State.IsActive) return;
         var NPCs = GameObject.Find("NPCs").transform;
         NPCs.Find("ToughBirdNPC (1)").gameObject.SetActive(false);
+        NPCs.Find("PolarBearNPC").gameObject.SetActive(false);
+        NPCs.Find("CampfireFriends").gameObject.SetActive(true);
+        new GameObject("Sidequel_ClimbersRemover").AddComponent<ClimbersRemover>();
+        RemoveItems();
+    }
+    private static void RemoveItems()
+    {
+        foreach (var item in GameObject.FindObjectsOfType<CollectOnTouch>())
+        {
+            // remove coins and feathers
+            item.gameObject.SetActive(false);
+        }
+        string[] removedItemPrefixes = [
+            "ShellPickup",
+            "ToyShovelPickup",
+        ];
+        var items = GameObject.FindObjectsOfType<CollectOnInteract>()
+            .Where(i => removedItemPrefixes.Any(p => i.name.StartsWith(p)));
+        foreach (var item in items)
+        {
+            item.gameObject.SetActive(false);
+        }
+        var shovel = GameObject.Find("Shovel");
+        if (shovel != null) shovel.SetActive(false);
+    }
+    private class ClimbersRemover : MonoBehaviour
+    {
+        private GameObject tim = null!;
+        private GameObject alex = null!;
+        private GameObject above = null!;
+        private void Awake()
+        {
+            var cutscenes = GameObject.Find("Cutscenes").transform;
+            tim = cutscenes.Find("RockClimberHangout/ClimbSquirrel").gameObject;
+            alex = cutscenes.Find("RockClimberHangout/GroundRhinoNPC").gameObject;
+            above = cutscenes.Find("RockClimbersAbove").gameObject;
+        }
+        private void Update()
+        {
+            if (tim != null && tim.activeSelf) tim.SetActive(false);
+            if (alex != null && alex.activeSelf) alex.SetActive(false);
+            if (above != null && above.activeSelf) above.SetActive(false);
+            GameObject.Destroy(gameObject);
+        }
     }
 }
 
