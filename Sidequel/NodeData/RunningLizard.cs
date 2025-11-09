a4d10f334deb3dca2af77c4b40f9297036f926f6
usuke1726 <78670588+usuke1726@users.noreply.github.com>
2025-11-09 17:09:27 +0900

    added nodes of runninglizard

---
+++ Sidequel/NodeData/RunningLizard.cs
@@ -0,0 +1,56 @@
+﻿
+using ModdingAPI;
+using Sidequel.Dialogue;
+
+namespace Sidequel.NodeData;
+
+internal class RunningLizard : NodeEntry
+{
+    internal const string BeforeJA1 = "RunningLizard.BeforeJA1";
+    internal const string BeforeJA2 = "RunningLizard.BeforeJA2";
+    internal const string BeforeJA3 = "RunningLizard.BeforeJA3";
+    internal const string AfterJA1 = "RunningLizard.AfterJA1";
+    internal const string AfterJA2 = "RunningLizard.AfterJA2";
+    internal const string GoldMedal1 = "RunningLizard.GoldMedal1";
+    internal const string GoldMedal2 = "RunningLizard.GoldMedal2";
+    internal const string GoldMedal3 = "RunningLizard.GoldMedal3";
+    protected override Characters? Character => Characters.RunningLizard;
+    protected override Node[] Nodes => [
+        new(BeforeJA1, [
+            lines(1, 7, digit2, [4, 5]),
+            done(),
+        ], condition: () => _bJA && NodeYet(BeforeJA1)),
+
+        new(BeforeJA2, [
+            lines(1, 6, digit2, [3, 4, 6]),
+            done(),
+        ], condition: () => _bJA && NodeDone(BeforeJA1) && NodeYet(BeforeJA2)),
+
+        new(BeforeJA3, [
+            lines(1, 4, digit2, [4]),
+        ], condition: () => _bJA && NodeDone(BeforeJA2)),
+
+        new(AfterJA1, [
+            lines(1, 9, digit2, [3, 6, 8]),
+            done(),
+        ], condition: () => _aJA && NodeYet(AfterJA1), priority: -1),
+
+        new(AfterJA2, [
+            lines(1, 4, digit2, []),
+        ], condition: () => NodeDone(AfterJA1), priority: -1),
+
+        new(GoldMedal1, [
+            lines(1, 11, digit2, [1, 2, 11]),
+            done(),
+        ], condition: () => NodeActive(Const.Events.GoldMedal) && NodeYet(GoldMedal1)),
+
+        new(GoldMedal2, [
+            lines(1, 3, digit2, [2]),
+        ], condition: () => NodeActive(Const.Events.GoldMedal) && NodeDone(GoldMedal1)),
+
+        new(GoldMedal3, [
+            lines(1, 2, digit2, []),
+        ], condition: () => NodeDone(Const.Events.GoldMedal)),
+    ];
+}
+
