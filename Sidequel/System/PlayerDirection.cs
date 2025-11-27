9a927c34b0d130057769831f3a7246a9df45e3af
usuke1726 <78670588+usuke1726@users.noreply.github.com>
2025-11-27 14:00:37 +0900

    modified to face the previous direction after closing an item prompt

---
+++ Sidequel/System/PlayerDirection.cs
@@ -0,0 +1,43 @@
+﻿
+using HarmonyLib;
+using ModdingAPI;
+using QuickUnityTools.Input;
+using UnityEngine;
+
+namespace Sidequel.System;
+
+[HarmonyPatch(typeof(Player))]
+internal class TurnToFacePatch
+{
+    private static Vector3 previousFaceToward;
+    [HarmonyPrefix()]
+    [HarmonyPatch("TurnToFace", [typeof(Transform)])]
+    internal static void TurnToFace(Transform target, Player __instance)
+    {
+        if (!State.IsActive) return;
+        previousFaceToward = __instance.transform.position + __instance.transform.forward.normalized * 10;
+    }
+    internal static void ResetFaceDirection()
+    {
+        Context.player.TurnToFace(previousFaceToward);
+    }
+}
+
+[HarmonyPatch(typeof(ItemPrompt))]
+internal class ItemPromptPatch
+{
+    [HarmonyPrefix()]
+    [HarmonyPatch("Update")]
+    internal static bool OnUpdate(ItemPrompt __instance)
+    {
+        if (!State.IsActive) return true;
+        var input = Traverse.Create(__instance).Field("input").GetValue<FocusableUserInput>();
+        if (input.WasDismissPressed())
+        {
+            TurnToFacePatch.ResetFaceDirection();
+            GameObject.Destroy(__instance.gameObject);
+        }
+        return false;
+    }
+}
+
