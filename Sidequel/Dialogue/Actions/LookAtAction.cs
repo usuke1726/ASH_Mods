b3f3fc55646e28a1eead1d4058a2499535b74958
usuke1726 <78670588+usuke1726@users.noreply.github.com>
2026-01-29 16:04:35 +0900

    modified to allow `Transform` as the target for the `lookAt` action

--- Sidequel/Dialogue/Actions/LookAtAction.cs
+++ Sidequel/Dialogue/Actions/LookAtAction.cs
@@ -1,20 +1,31 @@
 ﻿
 using System.Collections;
+using UnityEngine;
 
 namespace Sidequel.Dialogue.Actions;
 
 internal class LookAtAction : BaseAction, IInvokableInAction
 {
     private readonly string character;
+    private readonly bool isStringTarget;
     private readonly string? target;
+    private readonly Func<Transform?> getTarget = null!;
     public LookAtAction(string character, string? target, string? anchor = null) : base(ActionType.LookAt, anchor)
     {
         this.character = character;
         this.target = target;
+        isStringTarget = true;
+    }
+    public LookAtAction(string character, Func<Transform?> getTarget, string? anchor = null) : base(ActionType.LookAt, anchor)
+    {
+        this.character = character;
+        this.getTarget = getTarget;
+        isStringTarget = false;
     }
     public override IEnumerator Invoke(IConversation conversation)
     {
-        LookAt(conversation, character, target);
+        if (isStringTarget) LookAt(conversation, character, target);
+        else LookAtTr(conversation, character, getTarget);
         yield break;
     }
     internal static void LookAt(IConversation conversation, string character, string? target)
@@ -23,7 +34,14 @@ internal class LookAtAction : BaseAction, IInvokableInAction
         ch.lookAt = (target != null && Character.TryGetCharacter(conversation, target, out var t)) ? t.transform : null;
         lookedCharacters.Add(ch);
     }
+    internal static void LookAtTr(IConversation conversation, string character, Func<Transform?> getTarget)
+    {
+        if (!Character.TryGetCharacter(conversation, character, out var ch)) return;
+        ch.lookAt = getTarget();
+        lookedCharacters.Add(ch);
+    }
     internal static void LookAt(string character, string target) => LookAt(null!, character, target);
+    internal static void LookAtTr(string character, Func<Transform?> getTarget) => LookAtTr(null!, character, getTarget);
 
     private static readonly HashSet<ModdingAPI.Character> lookedCharacters = [];
     internal static void CleanUp()
